807784cb5d28f184b13c9a372d0484bd2815c9e7
Charlie O'Hara <charlie.b.ohara@gmail.com>
2017-12-16 18:24:41 -0800

    refactor with list comp

--- negate.py
+++ negate.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
 
 def negate(matrix):
-	output = []
-	for row in matrix:
-		out_row = []
-		for number in row:
-			if number < 0:
-				out_row.append(abs(number))
-			if number > 0:
-				out_row.append(number - (number * 2))
-		output.append(out_row)
-	return output
+	return [-n for n in row [for row in matrix]]
