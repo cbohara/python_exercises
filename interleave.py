bdaf231954ade1ba6ccd3bc13c76da1636d72594
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-01-07 19:56:58 -0800

    bonus

--- interleave.py
+++ interleave.py
@@ -2,12 +2,16 @@
 
 def interleave(*iterables):
 	iterables = [list(x) for x in iterables]
+	longest_iterable = sorted(iterables, reverse=True)[0]
 	result = []
-	for x in range(len(iterables[0])):
+	for x in range(len(longest_iterable)):
 		for iterable in iterables:
-			result.append(iterable[x])
+			try:
+				result.append(iterable[x])
+			except IndexError:
+				continue
 	return result
 
-nums = [1, 2, 3]
-squares = (n*n for n in nums)
-print(interleave(nums, squares))
+list1 = [1, 2, 3]
+list2 = [4, 5, 6, 7, 8]
+print(interleave(list1, list2))
--- test_interleave.py
+++ test_interleave.py
@@ -11,3 +11,14 @@ def test_list_and_generator_comprehension():
 	nums = [1, 2, 3, 4]
 	squares = (n**2 for n in nums)
 	assert interleave(nums, squares) == [1, 1, 2, 4, 3, 9, 4, 16]
+
+def test_three_arguments():
+	list1 = [1, 2, 3]
+	list2 = [4, 5, 6]
+	list3 = [7, 8, 9]
+	assert interleave(list1, list2, list3) == [1, 4, 7, 2, 5, 8, 3, 6, 9]
+
+def test_two_arguments_alternative_lengths():
+	list1 =[1, 2, 3]
+	list2 = [4, 5, 6, 7, 8]
+	assert interleave(list1, list2) == [1, 4, 2, 5, 3, 6, 7, 8]
