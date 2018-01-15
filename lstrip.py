57d3e3baafbb2c25c72fcc17114342b0e2ca3bd8
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-01-14 17:02:17 -0800

    ugly ugly attempt :)

--- lstrip.py
+++ lstrip.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
 def lstrip(iterable, obj):
-	return [x for x in iterable if x != obj]
+	start = 0
+	iterable = list(iterable)
+	for index, value in enumerate(iterable):
+		if value == obj:
+			continue
+		else:
+			start = index
+			break
+	try:
+		if start == 0 and iterable[0] == obj:
+			return []
+	except IndexError:
+		return []
+	else:
+		return iterable[start:]
--- test_lstrip.py
+++ test_lstrip.py
@@ -5,11 +5,17 @@ from lstrip import lstrip
 def test_list():
 	assert lstrip([0, 0, 1, 2, 3], 0) == [1, 2, 3]
 
+def test_object_later_in_list():
+	assert lstrip([0, 0, 1, 2, 0, 3], 0) == [1, 2, 0, 3]
+
 def test_nothing_strip():
 	assert lstrip([1, 2, 3], 0) == [1, 2, 3]
 
 def test_string():
 	assert lstrip('  hello', ' ') == ['h', 'e', 'l', 'l', 'o']
+
+def test_string_with_space_later_in_string():
+	assert lstrip('  hel lo', ' ') == ['h', 'e', 'l', ' ', 'l', 'o']
 
 def test_empty_iterable():
 	assert lstrip([], 1) == []
