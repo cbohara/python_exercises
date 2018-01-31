b7f37253cd095b03f38d04735b6353a76617c976
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-01-30 18:57:30 -0800

    leverage deque

--- tail.py
+++ tail.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+from collections import deque
 
 def tail(iterable, n):
-	if n <= 0:
-		return []
-	return list(iterable)[-n:]
+    """Return the last n items of given iterable."""
+    if n <= 0:
+        return []
+    return list(deque(iterable, maxlen=n))
