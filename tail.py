fdaf323f58426d803a492b348392125989e1fbbc
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-02-02 07:51:25 -0800

    reviewing alt method

--- tail.py
+++ tail.py
@@ -2,7 +2,7 @@
 from collections import deque
 
 def tail(iterable, n):
-    """Return the last n items of given iterable."""
-    if n <= 0:
-        return []
-    return list(deque(iterable, maxlen=n))
+	"""Return the last n items of given iterable."""
+	if n <= 0:
+		return []
+	return list(deque(iterable, maxlen=n))
