ce10882c0b350a59a526b2aa1a7a997447b9546d
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-03-09 16:43:18 -0800

    bonus

--- deep_add.py
+++ deep_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
+from numbers import Number
 
-def deep_add(list_or_number):
-	total = 0
-	if type(list_or_number) == list:
-		for x in list_or_number:
-			total += deep_add(x)
-		return total
+def deep_add(iterable_or_number, start=0):
+	try:
+		iter(iterable_or_number)
+	except TypeError:
+		return iterable_or_number
 	else:
-		return list_or_number
+		return sum((deep_add(x) for x in iterable_or_number), start)
--- test_deep_add.py
+++ test_deep_add.py
@@ -26,36 +26,15 @@ def test_other_numeric_types():
 	assert deep_add([Decimal('5.6'), 2]) == Decimal('7.6')
 	assert deep_add([[Fraction(1)], Fraction(2)]) == Fraction(3)
 
-# To test the Bonus part of this exercise, comment out the following line
-@pytest.mark.skip(reason='bonus')
 def test_other_iterables():
 	numbers = [1, 2, 3, 4]
 	cubes_and_squares = ((n, (n**3, n**2)) for n in numbers)
 	assert deep_add(cubes_and_squares) == 140
 	assert deep_add([(1, 2), [3, {4, 5}]]) == 15
 
-# To test the Bonus part of this exercise, comment out the following line
-@pytest.mark.skip(reason='bonus')
 def test_start_value():
 	numbers = [1, 2, 3, 4]
 	assert deep_add(numbers, 0) == 10
 	assert deep_add(numbers, 1) == 11
 	assert deep_add(numbers, start=1) == 11
 	assert deep_add([[], []], start=-10) == -10
-
-# To test the Bonus part of this exercise, comment out the following line
-@pytest.mark.skip(reason='bonus')
-def test_pseudonumeric_types():
-	assertEqual(deep_add([timedelta(1)], timedelta(0)), timedelta(1))
-	class Num:
-		def __init__(self, val=0):
-			val = val
-		def __add__(self, other):
-			if isinstance(other, Num):
-				return Num(val + other.val)
-			else:
-				return Num(val + other)
-		__radd__ = __add__
-		def __eq__(self, other):
-			return val == other.val
-	assertEqual(deep_add([[Num(1)], Num(2)]), Num(3))
