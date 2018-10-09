8e66855e5376ccd3b814bf698b0deecc4e353928
Charlie O'Hara <charlie.b.ohara@gmail.com>
2018-10-08 20:18:01 -0700

    refactor using zip

--- previous.py
+++ previous.py
@@ -1,7 +1,2 @@
 def with_previous(sequence):
-	output = []
-	previous = None
-	for index, item in enumerate(sequence):
-		output.append((item, previous))
-		previous = item
-	return output
+	return zip(sequence, [None, *sequence])
