7023aca2c625cf5553e950277ccb518c5d148f0a
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-04-01 15:26:41 -0700

    refactor to use zip

--- add.py
+++ add.py
@@ -1,3 +1,4 @@
+"""
 def add(matrix1, matrix2):
 	output = []
 	for i, inner_matrix in enumerate(matrix1):
@@ -8,3 +9,23 @@ def add(matrix1, matrix2):
 			added.append(current_value + other_value)
 		output.append(added)
 	return output
+"""
+
+def add(matrix1, matrix2):
+	output = []
+	for index in range(len(matrix1)):
+		combos = list(zip(matrix1[index], matrix2[index]))
+		sums = [sum(value) for value in combos]
+		output.append(sums)
+	return output
+
+matrix1 = [
+	[1, -2],
+	[-3, 4]
+]
+matrix2 = [
+	[2, -1],
+	[0, -1]
+]
+#output = [[3, -3], [-3, 3]]
+add(matrix1, matrix2)
--- test_add.py
+++ test_add.py
@@ -10,3 +10,9 @@ def test_add_two_matrix_three_inner():
 	matrix1 = [[1, -2, 3], [-4, 5, -6], [7, -8, 9]]
 	matrix2 = [[1, 1, 0], [1, -2, 3], [-2, 2, -2]]
 	assert add(matrix1, matrix2) == [[2, -1, 3], [-3, 3, -3], [5, -6, 7]]
+
+def test_add_three_matrix_two_inner():
+	matrix1 = [[1, 9], [7, 3]]
+	matrix2 = [[5, -4], [3, 3]]
+	matrix3 = [[2, 3], [-3, 1]]
+	assert add(matrix1, matrix2, matrix3) == [[8, 8], [7, 7]]
