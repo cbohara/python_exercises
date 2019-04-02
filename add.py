5cdf2d0e2c9e1f061376eb051585e0989cafe7b2
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-04-02 12:45:34 -0700

    working for 2 matrices

--- add.py
+++ add.py
@@ -1,16 +1,3 @@
-"""
-def add(matrix1, matrix2):
-	output = []
-	for i, inner_matrix in enumerate(matrix1):
-		added = []
-		for j, value in enumerate(inner_matrix):
-			current_value = inner_matrix[j]
-			other_value = matrix2[i][j]
-			added.append(current_value + other_value)
-		output.append(added)
-	return output
-"""
-
 def add(matrix1, matrix2):
 	output = []
 	for index in range(len(matrix1)):
@@ -18,14 +5,3 @@ def add(matrix1, matrix2):
 		sums = [sum(value) for value in combos]
 		output.append(sums)
 	return output
-
-matrix1 = [
-	[1, -2],
-	[-3, 4]
-]
-matrix2 = [
-	[2, -1],
-	[0, -1]
-]
-#output = [[3, -3], [-3, 3]]
-add(matrix1, matrix2)
