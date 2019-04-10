ac4c6ec43372fefb2ea200fc2e01ab2d392f1fb0
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-04-10 10:01:26 -0700

    allow add to accept multiple matrices

--- add.py
+++ add.py
@@ -1,6 +1,6 @@
-def add(matrix1, matrix2):
+def add(*matrices):
 	"""Add corresponding numbers given 2-D matrices"""
 	return [
-		[n + m for n, m in zip(row1, row2)]
-		for row1, row2 in zip(matrix1, matrix2)
+		[sum(values) for values in zip(*rows)]
+		for rows in zip(*matrices)
 	]
