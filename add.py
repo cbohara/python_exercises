5581a246696a6a52b8dd0e5daeffccfa1700708e
Charlie O'Hara <charlie.b.ohara@gmail.com>
2019-06-19 09:18:50 -0700

    finish final bonus

--- add.py
+++ add.py
@@ -1,6 +1,11 @@
+from itertools import zip_longest
+
 def add(*matrices):
 	"""Add corresponding numbers given 2-D matrices"""
-	return [
-		[sum(values) for values in zip(*rows)]
-		for rows in zip(*matrices)
-	]
+	try:
+		return [
+			[sum(values) for values in zip_longest(*rows)]
+			for rows in zip_longest(*matrices)
+		]
+	except TypeError as e:
+		raise ValueError("Given matrices are not the same size") from e
